54f80913ff63c4dd9682572c263e0823c17322ad
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-28 13:15:09 +0530

    create user model for user table

---
+++ src/models/user.model.ts
@@ -0,0 +1,37 @@
+import { DataTypes, ModelDefined } from "sequelize";
+import { sequelize } from "../utils/connectDb";
+
+interface IBaseUserSchema {
+  id?: number;
+  name: string;
+  email: string;
+  age: number;
+}
+
+const userModel: ModelDefined<
+  IBaseUserSchema,
+  Omit<IBaseUserSchema, "id">
+> = sequelize.define(
+  "user",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    age: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "users",
+  }
+);
+
+export default userModel;
--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -1,16 +1,18 @@
 import { Sequelize } from "sequelize";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "../config";
 
+export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  dialect: "mysql",
+});
+
 export const connectDb = async () => {
   try {
-    const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-      host: DB_HOST,
-      port: Number(DB_PORT),
-      dialect: "mysql",
-    });
-
     await sequelize.authenticate();
     console.log("[::] Database connected ...");
+    await sequelize.sync();
+    console.log("[::] Database synchronized ...");
   } catch (error) {
     console.log("[!] Database not connected !!!");
   }
