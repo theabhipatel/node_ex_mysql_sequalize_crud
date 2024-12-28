b6b8887b23efcf7ef8c9213042bccaa9bc6cdcd4
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-28 14:01:51 +0530

    feat created api for update user by id

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -53,6 +53,22 @@ export const handleGetUserById: RequestHandler = async (req, res, next) => {
 };
 export const handleUpdateUserById: RequestHandler = async (req, res, next) => {
   try {
+    const id = req.params.id;
+    const user = await userModel.findByPk(id);
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+      return;
+    }
+    const { name, email, age } = req.body;
+    await user.update({ name, email, age });
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully.",
+      user,
+    });
   } catch (error) {
     next(error);
   }
